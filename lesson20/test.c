15c4ce8b7242759f42af2aeba352de7cc38ab6d9
Lotso <3208137151@qq.com>
2025-12-19 09:59:36 +0800

    add

---
+++ lesson20/test.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+
+
+//// 正常退出，退出码 0
+//int main() 
+//{
+//    printf("程序正常执行完毕\n");
+//    return 0;
+//}
+//
+//
+//int main() 
+//{
+//    printf("执行exit退出\n");
+//    exit(1);  // 退出码1，标识执行失败,也可以使用其他的退出码
+//}
+//
+//int main() 
+//{
+//    printf("执行_exit退出");
+//    // 故意不带 \n,体会刷没刷新缓冲区
+//    _exit(1);  // 退出码1，标识执行失败,也可以使用其他的退出码
+//}
+//
+
+// exit会冲刷缓存，输出"hello"
+void TestExit() 
+{
+    printf("hello");
+    exit(0);
+}
+
+// _exit不冲刷缓存，无输出
+void Test_Exit() 
+{
+    printf("hello");
+    _exit(0);
+}
+
+
+int main()
+{
+    //TestExit();
+    Test_Exit();
+    return 0;
+}
+
+
