56cdc744b36518181eb7803e40a18b2d27b071b3
Lotso <3208137151@qq.com>
2026-01-28 19:31:38 +0800

    手动实现一个简易的shell命令行解释器

---
+++ lesson22/myshell.c
@@ -0,0 +1,173 @@
+#include "myshell.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+// 提示符相关
+static char username[32];
+static char hostname[64];
+static char cwd[256];
+static char commandLine[256];
+// 与命令行相关
+static char* argv[64];
+static int argc = 0;
+static const char* sep = " ";
+// 与退出码有关
+static int lastCode = 0;
+// 与环境变量相关,按道理来说是由bash来维护的,从系统配置文件读,但是我们这里直接从系统bash拷贝就行了
+static char* env[64];
+
+static void GetUserName()
+{
+    char* _username = getenv("USER");
+    strcpy(username, (_username ? _username : "None"));
+}
+
+static void GetHostName()
+{
+    char* _hostname = getenv("HOSTNAME");
+    strcpy(hostname, (_hostname ? _hostname : "None"));
+}
+
+static void GetCmd()
+{
+   // char* _cwd = getenv("PWD");
+   //  strcpy(cwd, (_cwd ? _cwd : "None"));
+   char _cwd[256];
+   getcwd(_cwd, sizeof(_cwd));
+   if(strcmp(_cwd, "/") == 0)
+   {
+       strcpy(cwd, _cwd);
+   }
+   else{
+       int end = strlen(_cwd) - 1;
+       while(end >= 0)
+       {
+           if(_cwd[end] == '/')
+           {
+               strcpy(cwd, &_cwd[end + 1]);
+               break;
+           }
+           end--;
+       }
+   }
+}
+
+static void PrintPromt()
+{
+    GetUserName();
+    GetHostName();
+    GetCmd();
+    printf("[%s@%s %s]# ",username, hostname, cwd);
+    fflush(stdout);
+}
+
+static void GetCommandLine()
+{
+    if(fgets(commandLine, sizeof(commandLine), stdin) != NULL)
+    {
+        commandLine[strlen(commandLine) - 1] = 0;
+    }
+}
+// 1. yes
+// 0. no, 普通命令，让后续的执行
+int CheckBuiltinAndExcute()
+{
+    int ret = 0;
+    if(strcmp(argv[0], "cd") == 0)
+    {
+        // 内键命令
+        ret = 1;
+        if(argc == 2) // 后面至少需要跟个东西
+        {
+            chdir(argv[1]);
+        }
+    }
+    else if(strcmp(argv[0], "echo") == 0)
+    {
+        ret = 1;
+        if(argc == 2)
+        {
+            if(argv[1][0] == '$')
+            {
+                if(strcmp(argv[1], "$?") == 0)
+                {
+                    printf("%d\n", lastCode);
+                    lastCode = 0;
+                }
+                else{
+                    // env 
+                }
+            }
+            else
+            {
+                printf("%s\n", argv[1]);
+            }
+        }
+    }
+
+    return ret;
+}
+
+void Excute()
+{
+    pid_t id = fork();
+    if(id < 0)
+    {
+        perror("fork");
+        return;
+    }
+    else if(id == 0)
+    {
+        // 子进程
+        // 程序替换
+        execvp(argv[0], argv);
+        exit(1);
+    }
+    else{
+        // 父进程
+        int status = 0;
+        pid_t rid = waitpid(id, &status, 0);
+        (void)rid;
+        lastCode = WEXITSTATUS(status);
+    }
+}
+static void ParseCommandLine()
+{
+     // 清空
+     argc = 0;
+     memset(argv, 0, sizeof(argv));
+     // 判空
+     if(strlen(commandLine) == 0) return;
+     // 分割
+     argv[argc] = strtok(commandLine, sep);
+     while((argv[++argc] = strtok(NULL, sep)));
+}
+
+void bash()
+{
+    while(1)
+    {
+        // 第一步: 输出提示命令行
+        PrintPromt();
+
+        // 第二步: 等待用户输入, 获取用户输入
+        GetCommandLine();
+
+        // 第三步: 解析字符串,"ls -a -l" -> "ls" "-a" "-l"
+        ParseCommandLine();
+
+        if(argc == 0)
+            continue;
+
+        // 第四步: 有些命令, cd echo env等等不应该让子进程执行
+        // 而是让父进程自己执行,这些是内建命令. bash内部的函数
+        if(CheckBuiltinAndExcute())
+            continue;
+
+        // 第五步: 执行命令
+        Excute();
+    }
+}
